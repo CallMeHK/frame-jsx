4f3711fb4bfe0edc7d2ae5d984c7e50986c6fdbd
Tyler Gregg <hkrazy888@gmail.com>
2019-04-03 01:30:01 -0600

    feat: added diffing for addition and sub of elts, working on replace

--- src/frame.js
+++ src/frame.js
@@ -1,5 +1,8 @@
 export default class h {
   static dom(type, props, children) {
+    if (typeof type !== "string") {
+      return type(props);
+    }
     if (arguments.length > 3) {
       children = [children];
       for (let i = 3; i < arguments.length; i++) {
@@ -14,27 +17,8 @@ export default class h {
     };
   }
 
-  static renderToDom(vnode, root) {
-    let elt = document.createElement(vnode.type);
-    if (vnode.props !== null) {
-      Object.keys(vnode.props).forEach(prop => {
-        prop === "className"
-          ? elt.setAttribute("class", vnode.props[prop])
-          : elt.setAttribute(prop, vnode.props[prop]);
-      });
-    }
-    if (typeof vnode.children === "string") {
-      elt.innerText = vnode.children;
-    } else if (typeof vnode.children === "undefined") {
-    } else {
-      vnode.children.forEach(child => {
-        h.renderToDom(child, elt);
-      });
-    }
-    root.appendChild(elt);
-  }
-
   static createElement(node) {
+    console.log(node);
     if (typeof node === "string") {
       return document.createTextNode(node);
     }
@@ -48,13 +32,13 @@ export default class h {
       });
     }
     if (typeof node.children === "string") {
-      console.log("str", node.children);
+      // console.log("str", node.children);
       $el.innerText = node.children;
     } else if (typeof node.children !== "undefined") {
-      console.log("arr", node.children);
+      // console.log("arr", node.children);
       node.children
         .map(elt => {
-          console.log(elt);
+          // console.log(elt);
           return h.createElement(elt);
         })
         .forEach(elt => $el.appendChild(elt));
@@ -62,16 +46,39 @@ export default class h {
     return $el;
   }
 
+  static changed(node1, node2) {
+    const nodeTypeCheck = node1.type !== node2.type;
+    let childTypeCheck = false
+    if (typeof node1.children !=="object" && node1.children !== node2.children){
+      childTypeCheck = true
+    }
+    return nodeTypeCheck || childTypeCheck;
+  }
+
+
+  //  NEED TO QUENE CHANGES, MANY ISSUES OTHERWISE
   static diff($parent, oldNode, newNode, index = 0) {
+
+    console.log("old",oldNode,"new", newNode);
+    // if there is no old node
     if (!oldNode) {
-      $parent.appendChild(createElement(newNode));
-    } else if (!newNode) {
+      $parent.appendChild(h.createElement(newNode));
+    } 
+    // if there is no new node
+    else if (!newNode) {
+      // debugger;
       $parent.removeChild($parent.childNodes[index]);
-    } else if (changed(newNode, oldNode)) {
-      $parent.replaceChild(createElement(newNode), $parent.childNodes[index]);
-    } else if (newNode.type) {
+    }
+    // if new and old do not have the same type OR they are not an array AND are not equivalent
+    else if (h.changed(oldNode, newNode)){
+      $parent.replaceChild(h.createElement(newNode), $parent.childNodes[index]);
+    }
+    // if there is a node with a type
+    else if (newNode.type && typeof newNode.children === "object") {
+      // get the length of both the old node and new node children array
       const newLength = newNode.children.length;
       const oldLength = oldNode.children.length;
+      // walk through each node
       for (let i = 0; i < newLength || i < oldLength; i++) {
         h.diff(
           $parent.childNodes[index],
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 /** @jsx h.dom */
 import h from "./frame";
 
-let root = document.getElementById("app");
+let $root = document.getElementById("app");
 
-const beer = props => {
+const Beer = props => {
   const imgUrl =
     "https://images.unsplash.com/photo-1553689895-6e1cd3957b63?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=633&q=80";
 
@@ -11,21 +11,42 @@ const beer = props => {
     <div>
       <h2 className="hi">Hello</h2>
       <img height="150" src={imgUrl} />
+      <br/>
+      <button id="add">Add</button>
+      <button id="sub">Subtract</button>
+      <br/>
+      <button id="replace">Replace</button>
+      
       <ul>
         {props.list.map(item => (
           <li>{item}</li>
         ))}
       </ul>
-      <button onClick="console.log('hi')">Click me</button>
     </div>
   );
 };
 
-//console.log(beer());
-
-// h.renderToDom(beer(), root);
-const soup = ["beer", "cheese", "soup"];
-const node = <beer list={soup} />;
-const vnode = beer({ list: soup });
-console.log(node);
-root.appendChild(h.createElement(vnode));
+let soup = ["beer", "cheese", "soup"];
+// const node = h.createElement(<Beer list={soup} />)
+// $root.appendChild(node);
+h.diff($root, null, <Beer list={soup} />);
+document.addEventListener("click", e => {
+  if (e.target.id === "add") {
+    h.diff($root, <Beer list={soup} />, <Beer list={[...soup, "BEER"]} />);
+    soup = [...soup, "BEER"];
+  } else if (e.target.id === "sub") {
+    h.diff(
+      $root,
+      <Beer list={soup} />,
+      <Beer list={soup.filter((elt, i) => i !== soup.length - 1)} />
+    );
+    soup = soup.filter((elt, i) => i !== soup.length - 1);
+  } else if (e.target.id === "replace") {
+    h.diff(
+      $root,
+      <Beer list={soup} />,
+      <Beer list={["MEGA BEER","Mega cheess","zoup"]} />
+    );
+    soup = ["MEGA BEER","Mega cheess","zoup"]
+  }
+});
