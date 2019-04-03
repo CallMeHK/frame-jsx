f8ebab29ada934ba8958ea3c53eb3bad1eecd237
Tyler Gregg <hkrazy888@gmail.com>
2019-04-02 20:45:22 -0600

    feat: remade virtual dom and starting to diff

--- src/frame.js
+++ src/frame.js
@@ -33,4 +33,53 @@ export default class h {
     }
     root.appendChild(elt);
   }
+
+  static createElement(node) {
+    if (typeof node === "string") {
+      return document.createTextNode(node);
+    }
+
+    const $el = document.createElement(node.type);
+    if (node.props !== null) {
+      Object.keys(node.props).forEach(prop => {
+        prop === "className"
+          ? $el.setAttribute("class", node.props[prop])
+          : $el.setAttribute(prop, node.props[prop]);
+      });
+    }
+    if (typeof node.children === "string") {
+      console.log("str", node.children);
+      $el.innerText = node.children;
+    } else if (typeof node.children !== "undefined") {
+      console.log("arr", node.children);
+      node.children
+        .map(elt => {
+          console.log(elt);
+          return h.createElement(elt);
+        })
+        .forEach(elt => $el.appendChild(elt));
+    }
+    return $el;
+  }
+
+  static diff($parent, oldNode, newNode, index = 0) {
+    if (!oldNode) {
+      $parent.appendChild(createElement(newNode));
+    } else if (!newNode) {
+      $parent.removeChild($parent.childNodes[index]);
+    } else if (changed(newNode, oldNode)) {
+      $parent.replaceChild(createElement(newNode), $parent.childNodes[index]);
+    } else if (newNode.type) {
+      const newLength = newNode.children.length;
+      const oldLength = oldNode.children.length;
+      for (let i = 0; i < newLength || i < oldLength; i++) {
+        h.diff(
+          $parent.childNodes[index],
+          newNode.children[i],
+          oldNode.children[i],
+          i
+        );
+      }
+    }
+  }
 }
--- src/index.js
+++ src/index.js
@@ -2,27 +2,30 @@
 import h from "./frame";
 
 let root = document.getElementById("app");
-const beer = () => {
-  const sayHi = () => {
-    console.log("hey there");
-  };
+
+const beer = props => {
   const imgUrl =
-  "https://images.unsplash.com/photo-1553689895-6e1cd3957b63?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=633&q=80";
+    "https://images.unsplash.com/photo-1553689895-6e1cd3957b63?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=633&q=80";
 
   return (
     <div>
       <h2 className="hi">Hello</h2>
       <img height="150" src={imgUrl} />
       <ul>
-        <li>beer</li>
-        <li>cheese</li>
-        <li>soup</li>
+        {props.list.map(item => (
+          <li>{item}</li>
+        ))}
       </ul>
-      <button onClick="sayHi()">Click me</button>
+      <button onClick="console.log('hi')">Click me</button>
     </div>
   );
 };
 
-console.log(beer());
+//console.log(beer());
 
-h.renderToDom(beer(), root);
+// h.renderToDom(beer(), root);
+const soup = ["beer", "cheese", "soup"];
+const node = <beer list={soup} />;
+const vnode = beer({ list: soup });
+console.log(node);
+root.appendChild(h.createElement(vnode));
